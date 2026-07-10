c004a0b4831109e3530291b95749eac488298020
ktanen <kevinbtanen@gmail.com>
2026-07-10 15:04:48 -0400

    Added RabbitMQ connection logic

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,21 @@
+import amqp from "amqplib";
+async function shutdown(conn: amqp.ChannelModel, signal: string) {
+  console.log(`received ${signal}, shutting down...`);
+  try {
+    await conn.close();
+  } catch (err) {
+    console.error("error closing connection:", err);
+  } finally {
+    process.exit(0);
+  }
+}
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection successful");
+  process.on("SIGINT", () => shutdown(conn, "SIGINT"));
+  process.on("SIGTERM", () => shutdown(conn, "SIGTERM"));
 }
 
 main().catch((err) => {
