2001705e5265496b289f0ba795bb077d7710e89e
ktanen <kevinbtanen@gmail.com>
2026-07-16 12:03:51 -0400

    Added a publish function that uses messagepack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,6 +1,7 @@
 import{ type ConfirmChannel } from "amqplib";
 import { rejects } from "assert";
 import { channel } from "diagnostics_channel";
+import {encode} from "@msgpack/msgpack"
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -23,8 +24,25 @@ export function publishJSON<T>(
     });
   });
     
+}
 
-
-    
-    
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const message = Buffer.from(encode(value));
+      const publishOptions = {
+        "contentType": "application/x-msgpack",
+    };
+  return new Promise((resolve, reject) => {
+    ch.publish(exchange, routingKey, message, publishOptions, function (err, _) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
 }
